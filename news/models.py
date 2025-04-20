4563327e5fa76c97cd72b069f8bd0a41b9e56fa7
Ravil Bikeev <ravil.bikeev@indeed-id.com>
2025-04-20 14:14:26 +0700

    News

---
+++ news/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    news = models.ManyToManyField(News, related_name="tags")
+
+    def __str__(self):
+        return self.name
