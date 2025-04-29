bfcbc2f2a7ce05a3920c2778d035c57773082e9e
Ravil Bikeev <ravil.bikeev@indeed-id.com>
2025-04-29 22:48:58 +0700

    Documents. Fix sidebar categories (filter)

--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Document, DOCUMENT_CATEGORIES
 
 
@@ -7,15 +8,22 @@ from .models import Document, DOCUMENT_CATEGORIES
 def document_list(request):
     selected_category = request.GET.get("category")
     documents = Document.objects.all()
+    query = request.GET.get("q")
 
     if selected_category:
         documents = documents.filter(category=selected_category)
+    if query:
+        documents = documents.filter(Q(title__icontains=query))
 
     categories = DOCUMENT_CATEGORIES
 
-    context = {
-        "documents": documents,
-        "categories": categories,
-        "selected_category": selected_category,
-    }
-    return render(request, "documents/document_list.html", context)
+    return render(
+        request,
+        "documents/document_list.html",
+        {
+            "documents": documents,
+            "categories": categories,
+            "query": query,
+            "selected_category": selected_category,
+        },
+    )
